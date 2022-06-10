09b5f5975577973f5e4d0e9fc8ff3725820ead55
lazyzsz <1125106511@qq.com>
2022-06-10 11:41:39 +0800

    first commit

---
+++ Index.php
@@ -0,0 +1,6 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$obj = new zhengsize\Test();
+$obj->test();
---
+++ src/zhengsize/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace zhengsize;
+
+
+class Test
+{
+    public function test()
+    {
+        echo 'hi zhengsize';
+    }
+}
